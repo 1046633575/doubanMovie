8dfa3c42ed5078d144a547a3436cbd18ee5ba182
1046633575 <1046633575@qq.com>
2019-10-31 13:31:32 +0800

    影片的收藏与取消

--- src/view/movieDetail/movieDetail.jsx
+++ src/view/movieDetail/movieDetail.jsx
@@ -2,13 +2,57 @@ import React,{Component} from 'react'
 import NavBar from '../../components/NavBar/NavBar'
 import {getMovieDetail, getMovieComment} from '../../axios'
 import Score from '../../components/Score/Score'
+import {Toast} from "antd-mobile";
 import './movieDetail.css'
 
 export default class MovieDetail extends Component {
 
     state = {
         data: {images: {small: ''}, rating: {average: ''}},
-        comments: []
+        comments: [],
+        collectionFlag : false
+    }
+
+    // 收藏或取消收藏 用到 localStorage
+    collection = (data) => {
+        // 组合一个对象，用于存储在 localStorage 中
+        const movieObj = {
+            id: data.id,
+            title: data.title,
+            img: data.images.small
+        }
+        // 获取到本地存储对象 localStorage
+        let arr = JSON.parse(localStorage.getItem('movies'))
+        if(arr !== null) {
+            // 说明已创建 localStorage 对象
+            // 遍历每一项，存在 id 相同的一项，删除； 不存在，添加
+            // findIndex 找出符合条件的项并返回索引
+            const index = arr.findIndex(item => item.id === movieObj.id)
+            if(index !== -1){
+                // 删除
+                arr.splice(index,1)
+                // Toast 提示
+                Toast.success('取消收藏', 1.5)
+                // 同步 state 熄灭收藏图标
+                this.setState({collectionFlag: false})
+            } else {
+                // 添加
+                arr.push(movieObj)
+                // Toast 提示
+                Toast.success('收藏成功', 1.5)
+                // 同步 state 点亮收藏图标
+                this.setState({collectionFlag: true})
+            }
+            // 同步 localStorage
+            localStorage.setItem('movies', JSON.stringify(arr))
+        } else{
+            // localStorage 对象未创建
+            let arr = []
+            arr.push(movieObj)
+            // 同步 state 点亮收藏图标
+            this.setState({collectionFlag: true})
+            localStorage.setItem('movies', JSON.stringify(arr))
+        }
     }
 
     // 整合电影详细信息并返回
@@ -54,10 +98,19 @@ export default class MovieDetail extends Component {
                 this.setState({comments: res.data.comments})
             }
         })
+        // 获取 localStorage 查看是否被收藏
+        const arr = JSON.parse(localStorage.getItem('movies'))
+        if(arr) {
+            const index = arr.findIndex(item => item.id === id)
+            if(index !== -1) {
+                // 表示已收藏
+                this.setState({collectionFlag: true})
+            }
+        }
     }
 
     render () {
-        const {data, comments} = this.state
+        const {data, comments, collectionFlag} = this.state
         return (
             <div>
                 <NavBar title={data.title}/>
@@ -67,7 +120,15 @@ export default class MovieDetail extends Component {
                     <div className="detail_content">
 
                         <div className="detail_top">
-                            <img className='top_left' src={data.images.small}/>
+                            <div className="top_left">
+                                <img src={data.images.small}/>
+                                {/*点击收藏*/}
+                                <div className="collection" onClick={() => this.collection(data)}>
+                                    {
+                                        collectionFlag  ? <img src='http://www.bgwm.fun/picture/images/shoucang_over.png'/> : <img src='http://www.bgwm.fun/picture/images/shoucang.png'/>
+                                    }
+                                </div>
+                            </div>
                             <div className="top_right">
                                 <h2>{data.title}</h2>
                                 {
